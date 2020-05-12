22189dcb13d4829e99458b5e589977b9e7081cec
jason <jason.sinc95@gmail.com>
2020-05-12 15:20:28 +0000

    stage 5 flask_blog

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from application import app
 
-app = Flask(__name__)
+if __name__=='__main__':
+	app.run(debug=True, host='0.0.0.0')
 
-@app.route('/')
-@app.route('/home')
-def home():
-	return render_template('home.html', title="home page")
 
 
